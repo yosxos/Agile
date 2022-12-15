36c2a618fc751314803ebee89b9928820cc423be
yasait <yassirx2012@gmail.com>
2022-12-15 17:02:37 +0100

    test dashboard and html page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ConnexionComponent } from './pages/connexion/connexion.component';
@@ -10,16 +9,20 @@ import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
 import { environment } from '../environments/environment';
 import { provideAuth,getAuth } from '@angular/fire/auth';
 import { provideFirestore,getFirestore } from '@angular/fire/firestore';
-
 import { ProfilComponent } from './pages/profil/profil.component';
 import { InscriptionComponent } from './pages/inscription/inscription.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MaterialModule} from './material.module';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { MatButtonModule } from "@angular/material/button";
+import { MatCardModule } from "@angular/material/card";
 @NgModule({
   declarations: [
     AppComponent,
     ConnexionComponent,
-    ProfilComponent
-    InscriptionComponent
+    ProfilComponent,
+    InscriptionComponent,
+    DashboardComponent
 
   ],
   imports: [
@@ -29,7 +32,13 @@ import { InscriptionComponent } from './pages/inscription/inscription.component'
     HttpClientModule,
     provideFirebaseApp(() => initializeApp(environment.firebase)),
     provideAuth(() => getAuth()),
-    provideFirestore(() => getFirestore())
+    provideFirestore(() => getFirestore()),
+    BrowserAnimationsModule,
+    MaterialModule,
+    MatCardModule,
+    MatButtonModule
+
+
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/pages/profil/profil.component.spec.ts
+++ src/app/pages/profil/profil.component.spec.ts
@@ -1,15 +1,20 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Auth } from '@angular/fire/auth';
+import { UserService } from 'src/app/services/user.service';
 
 import { ProfilComponent } from './profil.component';
 
 describe('ProfilComponent', () => {
   let component: ProfilComponent;
   let fixture: ComponentFixture<ProfilComponent>;
-
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ ProfilComponent ]
-    })
+ let AuthStub: Partial<Auth>;
+ let userSup:Partial<UserService>
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports:[],
+      providers:[{ provide: UserService, useValue: userSup },
+        { provide: Auth, useValue: AuthStub },
+      ]})
     .compileComponents();
 
     fixture = TestBed.createComponent(ProfilComponent);
